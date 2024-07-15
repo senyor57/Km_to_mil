78e837d795c4f4d148ec895a7abeb2dcb313f788
senyor57 <93437220+senyor57@users.noreply.github.com>
2024-07-15 13:38:37 +0300

    Km_to_Mil
    Km'yi mil'e cevirme

---
+++ km yi mile cevirme.py
@@ -0,0 +1,9 @@
+# 1 KM = 0,621371192 mil
+
+
+carpan_deger = 0.621371192
+km = float(input("Lütfen km cinsinden değeri giriniz: "))
+
+mil = km * carpan_deger
+
+print("{} km {} mil değerine eşittir".format(km, mil))
